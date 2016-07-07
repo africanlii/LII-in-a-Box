fbe059a08655048b9c03a35f8a9b485b7b199082
fingers10 <africanlii@280410.no-reply.drupal.org>
2016-07-07 15:20:06 +0200

    adding the changes from indigo module

---
+++ modules/custom/indigo/templates/node--indigo-document.tpl.php
@@ -0,0 +1,10 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
+  <div class="node-inner">
+    <div class="content">
+      <?php 
+        print $content['indigo'];
+       ?>
+    </div>
+    
+  </div>
+</article>
